4ed3046684d2580fa01ca7cf11af78055d5627e8
Stephen Goss <stephengs94@gmail.com>
2023-02-07 18:56:50 -0600

    begin adding terrain

---
+++ src/main/java/engine/terrain/Terrain.java
@@ -0,0 +1,61 @@
+package engine.terrain;
+
+import engine.graphics.Material;
+import engine.graphics.Mesh;
+import engine.graphics.model.OBJLoader;
+
+public class Terrain {
+
+    private static final float SIZE = 800;
+    private static final int VERTEX_COUNT = 128;
+
+    private float x,z;
+    private Mesh model;
+    private Material texture;
+
+    public Terrain(int gridX, int gridZ, OBJLoader loader, Material material){
+        this.texture = material;
+        this.x = gridX;
+        this.z = gridZ;
+
+    }
+
+    private Mesh generateTerrain(OBJLoader loader){
+        int count = VERTEX_COUNT * VERTEX_COUNT;
+        float[] vertices = new float[count * 3];
+        float[] normals = new float[count * 3];
+        float[] textureCoords = new float[count*2];
+        int[] indices = new int[6*(VERTEX_COUNT-1)*(VERTEX_COUNT-1)];
+        int vertexPointer = 0;
+        for(int i=0;i<VERTEX_COUNT;i++){
+            for(int j=0;j<VERTEX_COUNT;j++){
+                vertices[vertexPointer*3] = (float)j/((float)VERTEX_COUNT - 1) * SIZE;
+                vertices[vertexPointer*3+1] = 0;
+                vertices[vertexPointer*3+2] = (float)i/((float)VERTEX_COUNT - 1) * SIZE;
+                normals[vertexPointer*3] = 0;
+                normals[vertexPointer*3+1] = 1;
+                normals[vertexPointer*3+2] = 0;
+                textureCoords[vertexPointer*2] = (float)j/((float)VERTEX_COUNT - 1);
+                textureCoords[vertexPointer*2+1] = (float)i/((float)VERTEX_COUNT - 1);
+                vertexPointer++;
+            }
+        }
+        int pointer = 0;
+        for(int gz=0;gz<VERTEX_COUNT-1;gz++){
+            for(int gx=0;gx<VERTEX_COUNT-1;gx++){
+                int topLeft = (gz*VERTEX_COUNT)+gx;
+                int topRight = topLeft + 1;
+                int bottomLeft = ((gz+1)*VERTEX_COUNT)+gx;
+                int bottomRight = bottomLeft + 1;
+                indices[pointer++] = topLeft;
+                indices[pointer++] = bottomLeft;
+                indices[pointer++] = topRight;
+                indices[pointer++] = topRight;
+                indices[pointer++] = bottomLeft;
+                indices[pointer++] = bottomRight;
+            }
+        }
+        return null; //loader.loadToVAO(vertices, textureCoords, normals, indices);
+    }
+
+}
